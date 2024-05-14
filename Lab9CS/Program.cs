8c76cf59aa3bdf52813a0bd6587775a613eeef0a
Shor <Shor@KOMPUTER>
2024-05-14 15:30:46 +0300

    Добавьте файлы проекта.

---
+++ Lab9CS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Car
+{
+    public string Model { get; set; }
+
+    public Car(string model)
+    {
+        Model = model;
+    }
+}
+
+class Garage
+{
+    private List<Car> cars = new List<Car>();
+
+    public void AddCar(Car car)
+    {
+        cars.Add(car);
+    }
+
+    public void WashAllCars(Washer washer)
+    {
+        foreach (Car car in cars)
+        {
+            washer.Wash(car);
+        }
+    }
+}
+
+class Washer
+{
+    public void Wash(Car car)
+    {
+        Console.WriteLine($"Моем машину: {car.Model}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car car1 = new Car("BMW");
+        Car car2 = new Car("Mazda");
+
+        Garage garage = new Garage();
+        garage.AddCar(car1);
+        garage.AddCar(car2);
+
+        Washer washer = new Washer();
+        garage.WashAllCars(washer);
+    }
+}
